36fdf728bbaea697b3968996117ca984b730992d
kyhnrn <rmnd88kw@gmail.com>
2025-09-30 22:04:29 +0900

    条件分岐編
    script.js

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ console.log(lists);
 
 const pullDownButton = document.getElementById("lists")
 console.log(pullDownButton)
+const pullDownParents = document.getElementById("pull-down")
 
 window.addEventListener('load',function(){
     const pullDownButton = this.document.getElementById("lists")
@@ -28,17 +29,24 @@ window.addEventListener('load',function(){
 
 
 pullDownButton.addEventListener("mouseover", function() {
-  console.log("乗る");
-  pullDownButton.setAttribute("style","background-color:blue;")
+  console.log("乗った時は青色");
+  this.setAttribute("style","background-color:blue;")
  })
 
 
 pullDownButton.addEventListener("mouseout", function() {
-  console.log("外れる");
-  pullDownButton.removeAttribute("style","background-color:red;")
+  console.log("外れた時は赤色");
+  this.removeAttribute("style","background-color:red;")
 });
 
 pullDownButton.addEventListener("click", function() {
-  console.log("クリック");
-  pullDownButton.setAttribute("style","background-color:green;")
+  if(pullDownParents.getAttribute("style")=="display:block;"){
+    pullDownParents.removeAttribute("style","display:block")
+    console.log("非表示")
+  } else{
+    pullDownParents.setAttribute("style","display:block;")
+    console.log("表示")
+  }
 });
+
+
